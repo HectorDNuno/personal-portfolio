2eb7e69acd4ee45eea95d33c814b3c5201dce72e
hector <hectornuno1994@gmail.com>
2022-08-04 16:33:25 -0700

    makes banner component have an 'animation' that looks like someone is typing, adds about section, let's connect button, and floating image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,70 @@
+import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import bannerImg from "../assests/img/satellite-solid.svg";
+
 export const Banner = () => {
-  return "Banner";
+  const [loopNumber, setLoopNumber] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 10);
+  const toRotate = ["Software Engineer", "Web Developer", "UI/UX Designer"];
+  const period = 2000;
+
+  const tick = () => {
+    let i = loopNumber % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNumber(loopNumber + 1);
+      setDelta(500);
+    }
+  };
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm Hector Nuno, `}
+              <div className="txt-rotate">
+                <span className="wrap">{text}</span>
+              </div>
+            </h1>
+            <p>About myself</p>
+            <button onClick={() => console.log("connect")}>
+              Let's Connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+
+          <Col xs={12} md={6} xl={5}>
+            <img src={bannerImg} alt="banner img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
