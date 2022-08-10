b47219893d5a6ffb72155f7ea3ea5e76a171a3d6
hector <hectornuno1994@gmail.com>
2022-08-09 17:10:40 -0700

    uses bootstrap tab and ProjectCard to display projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,61 @@
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+
 export const Projects = () => {
-  return <>these are my projects</>;
+  const projects = [
+    {
+      title: "Type Checkr",
+      description: "A reference for users who play the video game Pokémon",
+      imgUrl: "",
+    },
+    {
+      title: "Nozama",
+      description: "An Amazon clone that lets users create logins, add items to their cart, and process payments",
+      imgUrl: "",
+    },
+    {
+      title: "Weather App",
+      description:
+        "A simple weather app where a user can look up a city and see it's detailed weather information and a weekly weather report",
+      imgUrl: "",
+    },
+  ];
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>My most recent projects!</p>
+            <Tab.Container id="project-tabs" defaultActiveKey="first">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+
+                <Tab.Pane eventKey="second">Lorem</Tab.Pane>
+                <Tab.Pane eventKey="third">Ipsum</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src="" alt="" />
+    </section>
+  );
 };
