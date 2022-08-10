881d3eaab5133a00f2cf0b1b245ff58fc49945f8
hector <hectornuno1994@gmail.com>
2022-08-09 18:05:41 -0700

    imports images for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,23 +1,26 @@
 import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import nozama from "../assests/img/nozama_screenshot.png";
+import typeCheckr from "../assests/img/type-checkr-screenshot.png";
+import weatherApp from "../assests/img/weather-app-screenshot.png";
 
 export const Projects = () => {
   const projects = [
     {
       title: "Type Checkr",
       description: "A reference for users who play the video game Pokémon",
-      imgUrl: "",
+      imgUrl: typeCheckr,
     },
     {
       title: "Nozama",
       description: "An Amazon clone that lets users create logins, add items to their cart, and process payments",
-      imgUrl: "",
+      imgUrl: nozama,
     },
     {
       title: "Weather App",
       description:
         "A simple weather app where a user can look up a city and see it's detailed weather information and a weekly weather report",
-      imgUrl: "",
+      imgUrl: weatherApp,
     },
   ];
   return (
@@ -28,17 +31,20 @@ export const Projects = () => {
             <h2>Projects</h2>
             <p>My most recent projects!</p>
             <Tab.Container id="project-tabs" defaultActiveKey="first">
-              <Nav variant="pills" defaultActiveKey="/home">
+              {/* <Nav variant="pills" defaultActiveKey="/home">
                 <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                  <Nav.Link eventKey="first">Deployed Projects</Nav.Link>
                 </Nav.Item>
+
                 <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                  <Nav.Link eventKey="second">In the Works</Nav.Link>
                 </Nav.Item>
+
                 <Nav.Item>
-                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                  <Nav.Link eventKey="third">Future Endeavors</Nav.Link>
                 </Nav.Item>
-              </Nav>
+              </Nav> */}
+
               <Tab.Content>
                 <Tab.Pane eventKey="first">
                   <Row>
