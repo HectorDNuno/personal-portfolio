3e29ba1823d10857d6ad842797ea048af20ccc24
hector <hectornuno1994@gmail.com>
2022-08-09 18:05:05 -0700

    removes commented code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,6 @@ import cssLogo from "../assests/img/css3-alt-brands.svg";
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
