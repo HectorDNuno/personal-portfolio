aea5c4a9f2d8ad3ed143e9b4c4c57a71653531cf
hector <hectornuno1994@gmail.com>
2022-08-11 12:22:59 -0700

    covers div in anchor tag that leads to project url

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,17 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, siteUrl }) => {
   return (
     <Col sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} alt="" />
-        <div className="proj-txtx">
-          <h4> {title} </h4>
-          <span> {description} </span>
+      <a className="projectCard" href={siteUrl} target="_blank" rel="noopener noreferrer">
+        <div className="proj-imgbx">
+          <img src={imgUrl} alt="" />
+          <div className="proj-txtx">
+            <h4> {title} </h4>
+            <span> {description} </span>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
   );
 };
